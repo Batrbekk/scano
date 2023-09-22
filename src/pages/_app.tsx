15869ba2d2a04c1a801ef9ead194c8be34982465
b.kuandyk <b.kuandyk@mycar.kz>
2023-09-22 06:26:15 +0600

    fix:  - fix types in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
-import { CookiesProvider } from "react-cookie";
+import { CookiesProvider } from 'react-cookie';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  return (
-    <CookiesProvider>
-      <Component {...pageProps} />
-    </CookiesProvider>
-  );
+  // @ts-ignore
+  return <CookiesProvider><Component {...pageProps} /></CookiesProvider>
 }
 export default appWithTranslation(MyApp);
---
+++ src/pages/main/addTheme.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+export const addTheme: NextPage = () => {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
