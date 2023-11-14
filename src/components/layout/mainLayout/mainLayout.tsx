52f850d5a0a6a18ac743a5387f5aa5bb20320ea8
b.kuandyk <b.kuandyk@mycar.kz>
2023-11-14 16:44:56 +0600

    feat:  - add show rootRoute in layout

--- src/components/layout/mainLayout/mainLayout.tsx
+++ src/components/layout/mainLayout/mainLayout.tsx
@@ -33,88 +33,93 @@ const defaultProps: MainLayoutProps = {
 export const mainLayout: React.FC<MainLayoutProps> = ({children, withPadding = defaultProps.withPadding}) => {
   const router = useRouter();
   const [path, setPath] = useState('');
+  const [routerRoot, setRouterRoot] = useState('');
 
   useEffect(() => {
     setPath(router.asPath.substring(1));
-  }, []);
+    setRouterRoot(router.asPath.split('/').filter(Boolean)[0]);
+  }, [router]);
 
   return (
     <div className="flex w-screen">
-      <div className="fixed h-screen bg-[#37475F] w-64 px-6 py-3">
-        <a href="/main/">
-          <Image src={Logo} alt="logo" />
-        </a>
-        <div className="mt-12">
-          <div className="mb-6">
-            <p className="text-[#6481AD] uppercase mb-5 text-xs font-['Montserrat',sans-serif] font-semibold">Аналитика</p>
-            <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'analytic') && 'bg-[#848F9F] rounded'}`} onClick={() => {
-              router.push('/analytic');
-            }}>
-              <Image src={(path === 'analytic') ? WhiteAnalytic : Analytic} alt="icon" />
-              <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'analytic') ? 'text-white' : 'text-[#6481AD]'}`}>Аналитика</p>
+      {routerRoot && (
+        <div className="fixed h-screen bg-[#37475F] w-64 px-6 py-3">
+          <a href="/main/">
+            <Image src={Logo} alt="logo" />
+          </a>
+          <div className="mt-12">
+            <div className="mb-6">
+              <p className="text-[#6481AD] uppercase mb-5 text-xs font-['Montserrat',sans-serif] font-semibold">Аналитика</p>
+              <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'analytic') && 'bg-[#848F9F] rounded'}`} onClick={() => {
+                router.push('/analytic');
+              }}>
+                <Image src={(path === 'analytic') ? WhiteAnalytic : Analytic} alt="icon" />
+                <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'analytic') ? 'text-white' : 'text-[#6481AD]'}`}>Аналитика</p>
+              </div>
             </div>
-          </div>
-          <div className="mb-6">
-            <p className="text-[#6481AD] uppercase mb-4 text-xs font-['Montserrat',sans-serif] font-semibold">Материалдар</p>
-            <Accordion
-              className="px-0"
-              itemClasses={{
-                indicator: "data-[open=true]:rotate-180"
-              }}
-            >
-              <AccordionItem
-                key="1"
-                aria-label="1"
-                className="text-[#6481AD] pl-2 [&_h2]:w-full [&_button]:p-0 [&_button]:justify-between"
-                startContent={
-                  <div className="flex items-center gap-x-4">
-                    <Image src={Copy} alt="icon" />
-                    <p className="font-['Montserrat',sans-serif] text-base font-semibold">Материалдар</p>
-                  </div>
-                }
-                indicator={({isOpen}) => (isOpen ?
-                    <div className="ml-auto">
-                      <Image src={Minus} alt="icon" />
-                    </div>
-                  :
-                    <div className="ml-auto">
-                      <Image src={Plus} alt="icon" />
-                    </div>
-                )}
+            <div className="mb-6">
+              <p className="text-[#6481AD] uppercase mb-4 text-xs font-['Montserrat',sans-serif] font-semibold">Материалдар</p>
+              <Accordion
+                className="px-0"
+                itemClasses={{
+                  indicator: "data-[open=true]:rotate-180"
+                }}
+                defaultExpandedKeys={[routerRoot]}
               >
-                <div className="border-l-2 border-[#6481AD] ml-2 mt-4 cursor-pointer" onClick={() => {
-                  router.push('/dashboard');
-                }}>
-                  <p className={`prose prose-sm font-['Montserrat',sans-serif] font-semibold ${path === 'dashboard' ? `text-white pl-2` : `text-[#6481AD]`}`}>— Барлығы</p>
-                </div>
-                <div className="border-l-2 border-[#6481AD] ml-2 pt-4 cursor-pointer">
-                  <p className="text-[#6481AD] prose prose-sm font-['Montserrat',sans-serif] font-semibold w-full">— Өңдеуден өтпегендер</p>
-                </div>
-              </AccordionItem>
-            </Accordion>
-            <div className={`pl-2 flex items-center gap-x-4 mt-2 cursor-pointer py-2 ${(path === 'message' || path === 'message/addMessage') && 'bg-[#848F9F] rounded'}`} onClick={() => {
-              router.push('/message');
-            }}>
-              <Image src={(path === 'message' || path === 'message/addMessage') ? WhiteMessage : Message} alt="icon" />
-              <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'message' || path === 'message/addMessage') ? 'text-white' : 'text-[#6481AD]'}`}>Хабарламалар</p>
-            </div>
-            <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'subscribe' || path === 'subscribe/addSubscribe') && 'bg-[#848F9F] rounded'}`} onClick={() => {
-              router.push('/subscribe');
-            }}>
-              <Image src={(path === 'subscribe' || path === 'subscribe/addSubscribe') ? WhiteNewMess : NewMess} alt="icon" />
-              <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'subscribe' || path === 'subscribe/addSubscribe') ? 'text-white' : 'text-[#6481AD]'}`}>Жазылымдар</p>
-            </div>
-            <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'journal') && 'bg-[#848F9F] rounded'}`} onClick={() => {
-              router.push('/journal');
-            }}>
-              <Image src={(path === 'journal') ? WhiteJournal : Journal} alt="icon" />
-              <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'journal') ? 'text-white' : 'text-[#6481AD]'}`}>Журнал</p>
-            </div>
-            <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'spam') && 'bg-[#848F9F] rounded'}`} onClick={() => {
-              router.push('/spam');
-            }}>
-              <Image src={(path === 'spam') ? WhiteSpam : Spam} alt="icon" />
-              <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'spam') ? 'text-white' : 'text-[#6481AD]'}`}>Спам</p>
+                <AccordionItem
+                  key="dashboard"
+                  aria-label="dashboard"
+                  className="text-[#6481AD] pl-2 [&_h2]:w-full [&_button]:p-0 [&_button]:justify-between"
+                  startContent={
+                    <div className="flex items-center gap-x-4">
+                      <Image src={Copy} alt="icon" />
+                      <p className="font-['Montserrat',sans-serif] text-base font-semibold">Материалдар</p>
+                    </div>
+                  }
+                  indicator={({isOpen}) => (isOpen ?
+                      <div className="ml-auto">
+                        <Image src={Minus} alt="icon" />
+                      </div>
+                      :
+                      <div className="ml-auto">
+                        <Image src={Plus} alt="icon" />
+                      </div>
+                  )}
+                >
+                  <div className="border-l-2 border-[#6481AD] ml-2 mt-4 cursor-pointer" onClick={() => {
+                    router.push('/dashboard');
+                  }}>
+                    <p className={`prose prose-sm font-['Montserrat',sans-serif] font-semibold ${path === 'dashboard' ? `text-white pl-2` : `text-[#6481AD]`}`}>— Барлығы</p>
+                  </div>
+                  <div className="border-l-2 border-[#6481AD] ml-2 pt-4 cursor-pointer">
+                    <p className="text-[#6481AD] prose prose-sm font-['Montserrat',sans-serif] font-semibold w-full">— Өңдеуден өтпегендер</p>
+                  </div>
+                </AccordionItem>
+              </Accordion>
+              <div className={`pl-2 flex items-center gap-x-4 mt-2 cursor-pointer py-2 ${(path === 'message' || path === 'message/addMessage') && 'bg-[#848F9F] rounded'}`} onClick={() => {
+                router.push('/message');
+              }}>
+                <Image src={(path === 'message' || path === 'message/addMessage') ? WhiteMessage : Message} alt="icon" />
+                <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'message' || path === 'message/addMessage') ? 'text-white' : 'text-[#6481AD]'}`}>Хабарламалар</p>
+              </div>
+              <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'subscribe' || path === 'subscribe/addSubscribe') && 'bg-[#848F9F] rounded'}`} onClick={() => {
+                router.push('/subscribe');
+              }}>
+                <Image src={(path === 'subscribe' || path === 'subscribe/addSubscribe') ? WhiteNewMess : NewMess} alt="icon" />
+                <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'subscribe' || path === 'subscribe/addSubscribe') ? 'text-white' : 'text-[#6481AD]'}`}>Жазылымдар</p>
+              </div>
+              <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'journal') && 'bg-[#848F9F] rounded'}`} onClick={() => {
+                router.push('/journal');
+              }}>
+                <Image src={(path === 'journal') ? WhiteJournal : Journal} alt="icon" />
+                <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'journal') ? 'text-white' : 'text-[#6481AD]'}`}>Журнал</p>
+              </div>
+              <div className={`pl-2 flex items-center gap-x-4 cursor-pointer py-2 ${(path === 'spam') && 'bg-[#848F9F] rounded'}`} onClick={() => {
+                router.push('/spam');
+              }}>
+                <Image src={(path === 'spam') ? WhiteSpam : Spam} alt="icon" />
+                <p className={`font-['Montserrat',sans-serif] text-base font-semibold ${(path === 'spam') ? 'text-white' : 'text-[#6481AD]'}`}>Спам</p>
+              </div>
             </div>
             <div className="flex flex-col mt-6">
               <p className="text-[#6481AD] uppercase mb-4 text-xs font-['Montserrat',sans-serif] font-semibold">Баптаулар</p>
@@ -123,10 +128,11 @@ export const mainLayout: React.FC<MainLayoutProps> = ({children, withPadding = d
                 itemClasses={{
                   indicator: "data-[open=true]:rotate-180"
                 }}
+                defaultExpandedKeys={[routerRoot]}
               >
                 <AccordionItem
-                  key="1"
-                  aria-label="1"
+                  key="setting"
+                  aria-label="setting"
                   className="text-[#6481AD] pl-2 [&_h2]:w-full [&_button]:p-0 [&_button]:justify-between"
                   startContent={
                     <div className="flex items-center gap-x-4">
@@ -174,7 +180,7 @@ export const mainLayout: React.FC<MainLayoutProps> = ({children, withPadding = d
             </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="ml-64 flex flex-col w-full h-screen justify-between bg-[#F8F9FB]">
         <div>
           <LayoutNavbar />
